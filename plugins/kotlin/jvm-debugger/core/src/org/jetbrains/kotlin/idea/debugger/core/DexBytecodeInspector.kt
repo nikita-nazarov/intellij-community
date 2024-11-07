300a197d540c2d9ca3098c32aa448cea2ca23547
Nikita Nazarov <nikitanazarov@google.com>
2024-11-07 14:46:10 +0000

    [kotlin] Make `DexBytecodeInspector#filterAlreadyExecutedTargets` suspendable

--- plugins/kotlin/jvm-debugger/core/src/org/jetbrains/kotlin/idea/debugger/core/DexBytecodeInspector.kt
+++ plugins/kotlin/jvm-debugger/core/src/org/jetbrains/kotlin/idea/debugger/core/DexBytecodeInspector.kt
@@ -17,7 +17,7 @@ interface DexBytecodeInspector {
         return false
     }
 
-    fun filterAlreadyExecutedTargets(
+    suspend fun filterAlreadyExecutedTargets(
         targets: List<KotlinMethodSmartStepTarget>,
         context: SmartStepIntoContext
     ): List<KotlinMethodSmartStepTarget> {
